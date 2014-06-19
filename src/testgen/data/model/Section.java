beef7045e5d98dfe16df22967f6910048024ca92
ula88 <ulanowakowska88@wp.pl>
2014-06-19 23:51:54 +0200

    proba

--- src/testgen/data/model/Section.java
+++ src/testgen/data/model/Section.java
@@ -3,7 +3,6 @@ package testgen.data.model;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
-import java.util.Random;
 
 public class Section {
 	private String name;
